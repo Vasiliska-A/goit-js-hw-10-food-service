0aad585dcdfd081dd72200e7312fb39fa5ad2c79
Vasylyna Andybur <vasylyna1206@yahoo.com>
2021-07-01 23:29:06 +0300

    made handlebars markup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import menuItemTmpl from './templates/menu-Item.hbs';
 import menuItems from './menu.json';
+import menuItemTmpl from './templates/menu-Item.hbs';
+
 import './sass/main.scss';
 
-console.log(menuTmpl(menu));
+const menuList = document.querySelector('.js-menu');
 
-const menuList = document.querySelector('js-menu');
+const markUp = menuItemTmpl(menuItems, { ingredients: [] });
 
-const menuMarkup = makeMenuMarkUp(menuItems);
-menuList.insertAdjacentHTML('beforeend', menuMarkup);
-
-function makeMenuMarkUp(menuItems) {
-  console.log(menuItems);
-  return menuItemTmpl(menuItems);
-}
+menuList.insertAdjacentHTML('afterbegin', markUp);
